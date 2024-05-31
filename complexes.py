7248a918f7f311508710d7e8818229573955f292
slavikyd <slavikyd@gmail.com>
2024-05-31 21:21:28 +0300

    added complex reactive resistance calculation funcs for inductivity and capacity

---
+++ complexes.py
@@ -0,0 +1,39 @@
+def get_xC(omega: float, capacitance: float, only_real_part: bool = False) -> complex | int:
+    """Calculator of the reactive resistance value for capacitor.
+
+    Args:
+        omega (float): angle acceleration value.
+        capacitance (float): energy capactiance value for capacitor
+        only_real_part: flag for development
+
+    Returns:
+        complex | int: complex or real part of the reactive resistance
+    """
+    DEFAULT_J = complex(0, 1)
+    react_resistance = (1 / (omega * capacitance)) * DEFAULT_J
+    if only_real_part:
+        return react_resistance.real
+    return react_resistance
+
+print(get_xC(10**4, 10**-5))
+
+
+def get_xC(omega: float, inductance: float, only_real_part: bool = False) -> complex | int:
+    """Calculator of the reactive resistance value for inductivity.
+
+    Args:
+        omega (float): angle acceleration value.
+        inductance (float): inductive energy value for inductor
+        only_real_part: flag for development
+
+    Returns:
+        complex | int: complex or real part of the reactive resistance
+    """
+    DEFAULT_J = complex(0, 1)
+    react_resistance = (1 / (omega * inductance)) * DEFAULT_J
+    if only_real_part:
+        return react_resistance.real
+    return react_resistance
+
+
+print(get_xC(10**4, 10**-5))
